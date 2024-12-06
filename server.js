b9889d72b0e25fc96069311098de6221cbc77cac
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-12-06 22:15:49 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Store users
+let users = [];
+
+app.use(express.static('public')); // Serve static files (like HTML, CSS, JS)
+
+// Serve the index.html (or your front-end file) for the root path
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle user joining the chat
+    socket.on('join', (username) => {
+        if (!username) return; // Make sure the username is provided
+        
+        console.log(`${username} joined the chat`);  // Log to terminal
+        users.push(username);
+
+        // Notify all clients that a new user has joined
+        io.emit('chatMessage', { name: 'System', message: `${username} joined the chat` });
+    });
+
+    // Handle chat message
+    socket.on('chatMessage', (data) => {
+        io.emit('chatMessage', data);
+    });
+
+    // Handle user disconnecting
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
